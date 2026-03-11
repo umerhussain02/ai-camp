347e89adb4fa3fface540f7610cd6d9471f542db
umerhussain02 <muhussain0898@gmail.com>
2026-03-11 20:20:29 +0500

    add class one

---
+++ class_01.py
@@ -0,0 +1,31 @@
+# Data types
+# String
+# Int
+# Float
+# Boolean
+
+# a = 10
+# b = 3
+# c = a + b
+# print(c)
+
+# Legal Variables
+# a
+# ali
+# a3
+# _b
+# chatApp
+
+# Illegal Variables
+# 1a
+# . ? %
+
+# a = "10 | "
+# b = 3
+# c = a * b
+# print(c)
+
+a = "10"
+b = 3
+c = a / b
+print(c)
