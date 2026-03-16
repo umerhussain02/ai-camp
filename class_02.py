2ff9479f6ac0b44306dd88297b047dd078dc99fd
umerhussain02 <muhussain0898@gmail.com>
2026-03-17 00:50:20 +0500

    add class 2

---
+++ class_02.py
@@ -0,0 +1,45 @@
+# name =  "alishbaaaaaaaaa"
+# name =  'alishba bori'
+# print(name)
+
+# Ambiguity of data
+# a =  8 * 4 / 2 + 5 - 3
+# a =  8 * 4 / (2 + (5 - 3))
+# print(a)
+
+# x =  int(input("Enter value: "))
+# y = 6
+# z = x + y
+# print(z)
+
+# Comparisition Operators
+# Equal to ==
+# Lessthan and equal to <=
+# Graterthan and equal to >=
+# Not equal to !=
+# Not !
+
+# Conditional statement
+
+# a = 5
+# if a < 3:
+#     print("Good")
+# # elif a == 5:
+# elif a != 5:
+#     print("Woww")
+# else:
+#     print("Bad")
+
+# Logical Operators
+# or || (If one condition is true)
+# and && (All conditions has to be true)
+# not !
+
+age = input("Enter your age: ")
+gender = input("Enter your Gender: ")
+
+if a > 3:
+    print("Good")
+else:
+    print("Bad")
+
